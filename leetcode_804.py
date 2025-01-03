414bd01beab1dfb2f9db4ce9038359bd452a17eb
student-om <155445191+student-om@users.noreply.github.com>
2025-01-03 22:58:54 +0530

    Create leetcode_804.py

---
+++ leetcode_804.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def uniqueMorseRepresentations(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        s='abcdefghijklmnopqrstuvwxyz'
+        d=[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        m={}
+        a=0
+        for i in s:
+            m[i]=d[a]
+            a+=1
+        s=set()
+        for  i in words:
+            x=''
+            for j in i:
+                x+=m[j]
+            s.add(x)
+        return len(s)
+            
