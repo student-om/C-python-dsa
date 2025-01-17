f60c6e91f908b62f3fa5424c073929b21b03710a
student-om <155445191+student-om@users.noreply.github.com>
2025-01-17 21:40:36 +0530

    Create 2683.py

---
+++ 2683.py
@@ -0,0 +1,6 @@
+class Solution:
+    def doesValidArrayExist(self, derived: List[int]) -> bool:
+        XOR = 0
+        for element in derived:
+            XOR = XOR ^ element
+        return XOR == 0
