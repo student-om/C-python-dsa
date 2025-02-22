d2c88e0a17115d49f1ea6fb35292ac8b24acb9b8
student-om <155445191+student-om@users.noreply.github.com>
2025-02-23 03:38:15 +0530

    Create isMirror.cpp

---
+++ isMirror.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void rotate(TreeNode *root){
+        if (!root)
+            return 
+        TreeNode *temp = root->right;
+        root->right= root->left;
+        root->left=temp;
+        rotate(root->left);
+        rotate(root->right);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        
+        T
+    }
+};
